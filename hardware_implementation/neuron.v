58ca12d4814338e4050794db3e4d7f4e5afad5a7
Joseph Attalla <jojoatt10@gmail.com>
2026-04-18 16:20:51 -0400

    add neuron.v, add neuron param to top

---
+++ hardware_implementation/neuron.v
@@ -0,0 +1,31 @@
+module neuron #(
+    parameter IN = 784
+) (
+    input [IN-1:0] weight,
+    input [IN-1:0] x,
+    output integer count
+);
+  wire [IN-1:0] mult;
+
+
+  assign mult = ~(weight ^ x);
+
+
+  integer popcount;
+  integer i;
+
+  always @(*) begin
+    popcount = 0;
+    for (i = 0; i < IN; i = i + 1) begin
+      if (mult[i]) begin
+        popcount = popcount + 1;
+      end
+    end
+  end
+
+  always @(*) begin
+    count = (popcount * 2) - IN;
+  end
+
+endmodule
+
