de9e01d185d29d9fd2b315756dbc77da3bcd7756
Joseph Attalla <jojoatt10@gmail.com>
2026-04-18 16:02:35 -0400

    add top module

---
+++ hardware_implementation/top.v
@@ -0,0 +1,75 @@
+`include "weight_mem.v"
+
+module top (
+    input  [783:0] image,
+    output [  3:0] classification
+);
+
+  // layer 1 weights
+  wire [783:0] w1[0:255];
+  initial begin
+    $readmemb("../models/weights_layer1.txt", weights);
+  end
+
+  // layer 2 weights
+  wire [255:0] w2[0:9];
+  initial begin
+    $readmemb("../models/weights_layer1.txt", weights);
+  end
+
+
+  // layer 1 neurons
+  integer [255:0] layer_1_neuron_outputs;
+  genvar i;
+  generate
+    for (i = 0; i < 256; i = i + 1) begin : g_layer_1_neurons
+      neuron layer_1_neurons (
+          .weight(w1[i]),
+          .x(image),
+          .out(layer_1_neuron_outputs)
+      );
+    end
+  endgenerate
+
+  // layer 1 activation
+  wire [255:0] layer_1_out;
+  generate
+    for (i = 0; i < 256; i = i + 1) begin : g_layer_1_activation
+      neuron layer_1_activation (
+          .neuron(layer_1_neuron_outputs[i]),
+          .out(layer_1_out[i])
+      );
+    end
+  endgenerate
+
+  // layer 2 neurons
+  integer [9:0] layer_2_neuron_outputs;
+  generate
+    for (i = 0; i < 10; i = i + 1) begin : g_layer_2
+      neuron layer_2 (
+          .weight(w2[i]),
+          .x(layer_1_out[i]),
+          .out(layer_2_neuron_outputs)
+      );
+    end
+  endgenerate
+
+  // get classification
+  integer j;
+  integer max_value;
+
+  always @(*) begin
+    max_value = layer_2_neuron_outputs[0];
+    max_index = 0;
+
+    for (j = 1; j < 10; j = j + 1) begin
+      if (layer_2_neuron_outputs[j] > max_value) begin
+        max_value = layer_2_neuron_outputs[j];
+        max_index = j;
+      end
+    end
+  end
+
+  assign classification = max_index;
+
+endmodule
